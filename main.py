92051c73cd6e06032065df457d8c13becbf5f4dc
ZachHill21 <814055@mystma.org>
2021-02-25 16:12:00 +0000

    try 1

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-bruh
+def factOrSum(x,o):
+    total = 1
+    sumtotal = x
+    if o == "factorial":
+        for i in range (1,x+1):
+            total *= i
+        return total
+    else:
+        for i in range (0, x):
+            sumtotal += i
+        return sumtotal
+
+
+
+print (factOrSum(3, "sum"))
